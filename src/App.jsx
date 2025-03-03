4966be1724faf1bb52cec32e6d5b5ee27bbfa37c
Ednial <96548579+Ednial@users.noreply.github.com>
2025-03-02 20:42:31 -0500

    new commit se agregan instrucciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 function App() {
-	return <div>
-		App
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing 
-			elit. Repellendus eos aspernatur officiis accusamus nihil 
-			ipsa voluptates fuga vel, itaque tempora sunt, error molestias 
-			similique quam accusantium autem ipsam nostrum architecto?</p>
-		</div>;
+	return (
+		<div>
+			App
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+				eos aspernatur officiis accusamus nihil ipsa voluptates fuga vel, itaque
+				tempora sunt, error molestias similique quam accusantium autem ipsam
+				nostrum architecto?
+			</p>
+			<button>boton</button>
+		</div>
+	);
 }
 
 export default App;
+
+{
+	/*CARLOS: Maquetar animaciones en el archivo index.css */
+}
