5f6a22b398306c2df3b37927b3b487b4eeb0aa09
Jorge_Gonzalez <108034786+Yorsdev@users.noreply.github.com>
2025-03-03 00:06:22 -0500

    Adition in Component App.jsx, Logic & Schema div container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
+import { useState } from 'react'
+import phrases from './data/phrases.json'
+import { __Phrase__Component__ } from './util/index.js'
+import __Phrase__Card__Component__ from './components/__Phrase__Card__Component__.jsx'
+import __Button__Component__ from './components/__Button__Component__.jsx'
+import { img1, img2, img3, img4 } from './assets'
+
+
+{/* Arreglo de imagenes */}
+
+const images = [img1, img2, img3, img4]
+
 function App() {
-	return (
-		<div>
-			App
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
-				eos aspernatur officiis accusamus nihil ipsa voluptates fuga vel, itaque
-				tempora sunt, error molestias similique quam accusantium autem ipsam
-				nostrum architecto?
-			</p>
-			<button>boton</button>
+	{ /* variables con el estado inicial y posterior */ }
+	const [phrase, setPhrase] = useState(__Phrase__Component__(phrases))
+	const [image, setImage] = useState(__Phrase__Component__(images))
+
+	// aqui se cambia el estado del componente
+	const changePhrases = () => {
+		setPhrase(__Phrase__Component__(phrases))
+		setImage(__Phrase__Component__(images))
+	}
+
+	return
+		{/*Se carga por defecto un background en estado inicial*/}
+		<div className="container" style={{ backgroundImage: `url('${image}')`}}>
+			<h1>Fortune cookie</h1>
+			
+			{/* Lo que va a contener el cardComponent  */}
+			<__Phrase__Card__Component__ 
+				phrase = {phrase.phrase}
+				author = {phrase.author}
+			/>
+
+			{/* Con funcion manejadora y parametro children dentro del componente */}
+			<__Button__Component__ handlerPhrase={changePhrases}>
+				Get a new phrase
+			</__Button__Component__>
+
 		</div>
-	);
 }
 
 export default App;
